82465b1be017109f6584888247d0aa4e36eaf4c2
ina <wenulife@gmail.com>
2022-11-07 19:55:28 +0200

    accepting failure...

--- 3/mnist_multi.py
+++ 3/mnist_multi.py
@@ -11,81 +11,94 @@ OUTPUT_SIZE = 10
 
 
 def get_params(hidden_size):
-    weights = np.array([np.array([rand.uniform(-0.1, 0.1) for _ in range(INPUT_SIZE)] for _ in range(hidden_size)),
-                        np.array([rand.uniform(-0.1, 0.1) for _ in range(hidden_size)] for _ in range(OUTPUT_SIZE))],
+    weights = np.array([np.array([[np.random.normal(loc=0.0, scale=1/math.sqrt(INPUT_SIZE)) for _ in range(INPUT_SIZE)] for _ in range(hidden_size)]),
+                        np.array([[np.random.normal(loc=0.0, scale=1/math.sqrt(hidden_size)) for _ in range(hidden_size)] for _ in range(OUTPUT_SIZE)])],
                        dtype=object)
-    biases = np.array([np.array(rand.uniform(-0.1, 0.1) for _ in range(hidden_size)),
-                       np.array(rand.uniform(-0.1, 0.1) for _ in range(OUTPUT_SIZE))], dtype=object)
-
-    print(weights)
+    biases = np.array([np.array([np.random.normal(loc=0.0, scale=1/math.sqrt(INPUT_SIZE)) for _ in range(hidden_size)]),
+                       np.array([np.random.normal(loc=0.0, scale=1/math.sqrt(hidden_size)) for _ in range(OUTPUT_SIZE)])], dtype=object)
 
     return weights, biases
 
 
 def sigmoid(values):
+    print(values)
     return np.array([1 / (1 + pow(math.e, -_)) for _ in values])
 
 
 def sigmoid_derivative(outputs):
-    return outputs * (1 - outputs)
+    return np.array([_ * (1 - _) for _ in outputs])
 
 
 def softmax(values):
     inputs_sum = sum(pow(math.e, value) for value in values)
-    return np.array([pow(math.e, value) / inputs_sum for value in values])
+    return np.array([pow(math.e, value) / (inputs_sum + pow(1, 0.00000001)) for value in values])
 
 
-def forward_prop(weights, biases, instance):
-    print(weights[0])
-    z0 = weights[0].dot(instance) + biases[0]
+def forward_prop(weights, biases, instances):
+    z0 = np.add(weights[0].dot(instances.transpose()), biases[0].reshape(100, 1))
+    print("jjjjjjjjjjjjjjjjjjjjjj", z0.shape)
     y0 = sigmoid(z0)
-    z1 = weights[1].dot(y0) + biases[1]
+    z1 = np.add(np.dot(weights[1], y0), biases[1].reshape(10, 1))
     y1 = softmax(z1)
     return z0, y0, z1, y1
 
 
-def get_one_hot(label):
+def forward_prop_instance(weights, biases, instance):
+    z0 = np.add(weights[0].dot(instance), biases[0])
+    y0 = sigmoid(z0)
+    z1 = np.add(np.dot(weights[1], y0), biases[1])
+    y1 = softmax(z1)
+    return z0, y0, z1, y1
+
+
+def get_one_hot_for_label(label):
     label_arr = np.array([0 for _ in range(OUTPUT_SIZE)])
     label_arr[label] = 1
-    return label_arr.transpose()
+    return label_arr
 
 
-def get_tuned_outputs(results):
-    outputs = np.array([sigmoid(result) for result in results])
+def get_one_hot(labels):
+    result = [np.array(get_one_hot_for_label(labels[0]))]
+    for i in range(1, len(labels)):
+        result = np.append(result, [get_one_hot_for_label(labels[i])], axis=0)
+
+    return result.transpose()
+
+
+def get_tuned_outputs(outputs):
+    print("aaaa",outputs)
     if np.count_nonzero(outputs == 1) == 1:
         return outputs.transpose()
 
-    outputs = np.array([0 for _ in range(OUTPUT_SIZE)])
-    outputs[np.argmax(results)] = 1
-    return outputs.transpose()
+    result = np.array([0 for _ in range(len(outputs))])
+    result[np.argmax(outputs)] = np.max(outputs)
+    return result.transpose()
 
 
-def backward_prop(z1, y0, y1, y2, weights_level_2, targets):
-    dz = np.array([[], []])
-    dw = np.array([[], []])
-    db = np.array([[], []])
-
+def backward_prop(z0, inputs, y1, y2, weights_level_2, targets):
     # dC/dy * dy/dz
-    # shape = 1, 10
-    dz[1] = 1 / len(targets) * np.sum(targets.subtract(y2))
+    # shape = 10, 50
+    dz2 = -1 / len(targets[0]) * np.subtract(targets, y2)
     # dy/dz * (error for every neuron * weights for it)
-    # shape = 1, 100
-    dz[0] = sigmoid_derivative(z1) * (dz[1].dot(weights_level_2))
+    # shape = 100, 50 * 100, 50(50, 10 dot 10, 100)  =>  100, 50
+    dz1 = sigmoid_derivative(z0) * dz2.transpose().dot(weights_level_2).transpose()
     # (hidden -> output)
-    # 10, 1 * 1, 100 = 10, 100
-    dw[1] = dz[1].transpose().dot(y1)
-    db[1] = dz[1]
+    # 10, 50 * 50, 100 = 10, 100
+    dw2 = dz2.dot(y1.transpose())
+    db2 = np.sum(dz2, axis=1)
     # (input -> hidden)
-    # 100, 1 * 1, 10 = 100, 10
-    dw[0] = dz[0].transpose().dot(y0)
-    db[0] = dz[0]
+    # 100, 50 * 50, 784 = 100, 784
+    dw1 = dz1.dot(inputs)
+    db1 = np.sum(dz1, axis=1)
 
-    return dz, db
+    return dw1, dw2, db1, db2
 
 
-def update_params(weights, biases, dw, db, learning_rate):
-    weights = weights - learning_rate * dw
-    biases = biases - learning_rate * db
+def update_params(weights, biases, dw1, dw2, db1, db2, learning_rate):
+    weights[0] = weights[0] - learning_rate * dw1
+    weights[1] = weights[1] - learning_rate * dw2
+    biases[0] = biases[0] - learning_rate * db1
+    biases[1] = biases[1] - learning_rate * db2
 
     return weights, biases
 
@@ -101,35 +114,31 @@ def get_loss_for_instance(target, output):
     return sum([target[i] * math.log(output[i]) for i in range(len(target))])
 
 
-def train(weights, biases, nr_epochs=6, learning_rate=0.4, batch_size=50):
-    batches_nr = TRAINING_SIZE // batch_size + 1
-
+def train(weights, biases, nr_epochs=5, learning_rate=0.3, batch_size=5000):
     for i in range(nr_epochs):
-        dw_delta, db_delta = np.array([([0] * 100) * 10]), np.array([([0] * 10) * 100])
-        for batch_start, batch_end in zip(range(0, batches_nr, batch_size), range(batch_size, batches_nr, batch_size)):
-            batch = [training_set[0][batch_start:batch_end], training_set[1][batch_start:batch_end]]
-            z0, y0, z1, y1 = forward_prop(weights, biases, batch)
-            dw, db = backward_prop(z0, y0, z1, y1, weights_level_2=weights[1], targets=batch[1])
-            dw_delta += dw
-            db_delta += db
-        for j in range(batches_nr):
-            weights, biases = update_params(weights, biases, dw_delta, db_delta, learning_rate)
+        for batch_start, batch_end in zip(range(0, TRAINING_SIZE, batch_size), range(batch_size, TRAINING_SIZE, batch_size)):
+            print(f"batch st :{batch_start}, {batch_end}")
+            batch = [np.array(training_set[0][batch_start:batch_end]), get_one_hot(training_set[1][batch_start:batch_end])]
+            z0, y0, z1, y1 = forward_prop(weights, biases, batch[0])
+            dw1, dw2, db1, db2 = backward_prop(z0, batch[0], y0, y1, weights_level_2=weights[1], targets=batch[1])
+            weights, biases = update_params(weights, biases, dw1, dw2, db1, db2, learning_rate)
 
     return weights, biases
 
 
-def get_accuracy(dataset):
+def get_accuracy(weights, biases, dataset):
     wrong_classified_nr = 0
     dataset_size = len(dataset[0])
 
     for i in range(dataset_size):
-        image_pixels = dataset[0][i]
         label = dataset[1][i]
-        results = np.add(np.dot(weights, image_pixels), biases)
-        outputs = get_tuned_outputs(results)
-        label_arr = get_one_hot(label)
+        print("WTFFFF")
+        print("WTFFFF")
+        z0, y0, z1, y1 = forward_prop_instance(weights, biases, dataset[0][i])
+        y1 = get_tuned_outputs(y1)
+        label_arr = get_one_hot_for_label(label)
 
-        if not np.array_equal(outputs, label_arr):
+        if not np.array_equal(y1, label_arr):
             wrong_classified_nr += 1
 
     return (dataset_size - wrong_classified_nr) / dataset_size * 100
@@ -144,5 +153,5 @@ if __name__ == '__main__':
 
     weights_main, biases_main = get_params(hidden_size=HIDDEN_SIZE)
     weights_main, biases_main = train(weights_main, biases_main)
-    print(f"the accuracy for the training set is: {get_accuracy(training_set)}")
-    print(f"the accuracy for the test set is: {get_accuracy(test_set)}")
+    # print(f"the accuracy for the training set is: {get_accuracy(weights_main, biases_main, training_set)}")
+    # print(f"the accuracy for the test set is: {get_accuracy(weights_main, biases_main, test_set)}")
